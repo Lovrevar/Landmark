f7083775d0776a791f2998c985afd5d758ce4d5b
Lovrevar <93528203+Lovrevar@users.noreply.github.com>
2025-10-14 15:20:03 +0200

    Updated SalesProjectsEnhanced.tsx

--- src/components/SalesProjectsEnhanced.tsx
+++ src/components/SalesProjectsEnhanced.tsx
@@ -532,7 +532,7 @@ const SalesProjectsEnhanced: React.FC = () => {
   const getUnitLabel = (unitType: UnitType) => {
     if (unitType === 'apartment') return 'Apartments'
     if (unitType === 'garage') return 'Garages'
-    return 'Repositoriess'
+    return 'Repository'
   }
 
   const calculateBulkPreview = () => {
