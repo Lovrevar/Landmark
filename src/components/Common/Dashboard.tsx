280c52ec05956077fb7c27c713ecf70a530e907e
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2026-03-04 12:12:28 +0100

    rename

--- src/components/Common/Dashboard.tsx
+++ src/components/Common/Dashboard.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useAuth } from '../../contexts/AuthContext'
-import DirectorDashboard from '../Dashboards/DirectorDashboard'
-import SalesDashboard from '../Dashboards/SalesDashboard'
-import SupervisionDashboard from '../Dashboards/SupervisionDashboard'
-import InvestmentDashboard from '../Dashboards/InvestmentDashboard'
-import AccountingDashboard from '../Dashboards/AccountingDashboard'
-import RetailDashboard from '../Dashboards/RetailDashboard'
+import DirectorDashboard from '../dashboards/DirectorDashboard'
+import SalesDashboard from '../dashboards/SalesDashboard'
+import SupervisionDashboard from '../dashboards/SupervisionDashboard'
+import InvestmentDashboard from '../dashboards/InvestmentDashboard'
+import AccountingDashboard from '../dashboards/AccountingDashboard'
+import RetailDashboard from '../dashboards/RetailDashboard'
 
 const Dashboard: React.FC = () => {
   const { currentProfile } = useAuth()
